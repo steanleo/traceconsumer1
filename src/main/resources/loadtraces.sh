983c53212d7ade3365ffb76b51a0298caaa146b5
steanleo <sa.leont94@gmail.com>
2021-12-15 19:50:56 +0100

    loadtraces changes

--- src/main/resources/loadtraces.sh
+++ src/main/resources/loadtraces.sh
@@ -1,3 +1,3 @@
 while IFS= read -r line; do
-curl -H "Content-Type: application/json" -X POST --data-binary "$line" http://localhost:8083/traces;
+curl -H "Content-Type: application/json" -X PUT --data-binary "$line" http://localhost:8083/traces;
 done < traces.json
